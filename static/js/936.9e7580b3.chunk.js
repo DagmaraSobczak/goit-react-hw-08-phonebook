05b2d53b43d6379b530c70d40c72d8c404dfc3a2
DagmaraSobczak <113237182+DagmaraSobczak@users.noreply.github.com>
2023-07-18 20:49:12 +0000

    Deploying to gh-pages from @ DagmaraSobczak/goit-react-hw-08-phonebook@a21210a4ccc25dc0e7ee7318750a39d00e81a4c6 🚀

---
+++ static/js/936.9e7580b3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[936],{4936:function(e,r,t){t.r(r),t.d(r,{default:function(){return s}});var n=t(6907),o="Home_box__BGP+V",c="Home_title__L087v",l=t(184),s=function(){return(0,l.jsxs)(n.B6,{children:[(0,l.jsx)(n.ql,{children:(0,l.jsx)("title",{children:"Phonebook"})}),(0,l.jsx)("div",{className:o,children:(0,l.jsx)("h1",{className:c,children:(0,l.jsx)("p",{children:"Create Your Phonebook "})})})]})}}}]);
+//# sourceMappingURL=936.9e7580b3.chunk.js.map
